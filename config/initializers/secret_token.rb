50edb9622c8ae981d3119eedbb015af9a30c0302
ckhrysze <git@ckhrysze.net>
2012-03-27 13:44:13 -0500

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mortals::Application.config.secret_token = '5e596e9963569a453d697c05cf8957c683c21c22742d483a4f211dd1e09e2626b075860c4cdb65c6443d5d0c9fd6145eb26d966ff6de83e3bcf4febbb0280dc0'
